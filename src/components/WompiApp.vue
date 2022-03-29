9381a7d0032f064e20be8f4ed4466b0e2b2ffbbf
Sergio <sergioa.orozcoo@gmail.com>
2022-03-29 17:55:28 -0500

    fix media queries behaviors in 2015px

--- src/components/WompiApp.vue
+++ src/components/WompiApp.vue
@@ -96,16 +96,10 @@ div {
 @media only screen and (min-width: 600px) {
   .is-fluid {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr 1fr;
     justify-content: center;
     align-items: center;
     gap: 4em;
-  }
-}
-
-@media only screen and (min-width: 768px) {
-  .is-fluid {
-    grid-template-columns: repeat(3, 1fr);
   }
 }
 
